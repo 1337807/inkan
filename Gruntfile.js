144df10ffc6a423e3279e64b167f2a02f862e137
max beizer <max.beizer@gmail.com>
2016-04-14 21:13:09 -0500

    Add stupid simple chrome live reload

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
     watch: {
       scripts: {
         files: ["./public/javascripts/*.js"],
-        tasks: ["browserify"]
+        tasks: ["browserify", "reload"]
       }
     }
   });
@@ -30,4 +30,12 @@ module.exports = function (grunt) {
 
   grunt.registerTask("default", ["watch"]);
   grunt.registerTask("build", ["browserify"]);
+  grunt.registerTask("reload", "reload Chrome on OS X",
+  function() {
+    require("child_process").exec("osascript " +
+    "-e 'tell application \"Google Chrome\" " +
+    "to tell the active tab of its first window' " +
+    "-e 'reload' " +
+    "-e 'end tell'");
+  });
 };
