c1b4fd854c069ca00095bd2b45fe549cfdb3fd45
max beizer <max.beizer@gmail.com>
2016-04-14 23:50:52 -0500

    Begin breaking out inputs into components

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,68 +1,15 @@
+import SignatureInput from './input.js'
+
 export default class SignatureForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state =  {name: '', title: '', email: ''};
-  }
-  handleNameChange(e) {
-    const value = e.target.value;
-    const { name, title, email }  = this.state;
-    this.setState({name: value})
-    this.enterContent({name: value, title: title, email: email});
-  }
-  handleTitleChange(e) {
-    const value = e.target.value;
-    const { name, title, email }  = this.state;
-    this.setState({title: value})
-    this.enterContent({name: name, title: value, email: email});
-  }
-  handleEmailChange(e) {
-    const value = e.target.value;
-    const { name, title, email }  = this.state;
-    this.setState({email: value})
-    this.enterContent({name: name, title: title, email: value});
-  }
-  enterContent(obj) {
-    this.props.contentEntered(obj);
+  handleContentEnter(content) {
+    this.props.contentEntered(content);
   }
   render() {
     return (
       <div className="purple-box u-padding-Al">
         <form>
-          <div className="form-group">
-            <label htmlFor="input-name">Name</label>
-            <input
-              type="text"
-              placeholder="Your name"
-              value={this.state.name}
-              onChange={this.handleNameChange.bind(this)}
-              className="form-control"
-              id="input-name"
-              />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="input-title">Title</label>
-            <input
-              type="text"
-              placeholder="Your Title"
-              value={this.state.title}
-              onChange={this.handleTitleChange.bind(this)}
-              className="form-control"
-              id="input-title"
-              />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="input-email">Email</label>
-            <input
-              type="text"
-              placeholder="Your Email"
-              value={this.state.email}
-              onChange={this.handleEmailChange.bind(this)}
-              className="form-control"
-              id="input-email"
-              />
-          </div>
+          <SignatureInput field="name" contentEntered={this.handleContentEnter.bind(this)} />
+          <SignatureInput field="title" contentEntered={this.handleContentEnter.bind(this)} />
         </form>
       </div>
     )
