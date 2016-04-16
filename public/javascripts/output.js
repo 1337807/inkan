9f7432c6808538c1d46a9fa3016d4eb17639d82c
max beizer <max.beizer@gmail.com>
2016-04-15 21:05:29 -0500

    Begin styling

--- public/javascripts/output.js
+++ public/javascripts/output.js
@@ -2,6 +2,14 @@ export default class SignatureOutput extends React.Component {
   render() {
     const data = this.props.data;
     const { name, title, email, twitter, linkedin, github, facebook } = data;
+    const theColorPurple = '#79589F';
+    const nameStyles = {
+      color: theColorPurple,
+      fontWeight: 'bolder'
+    };
+    const emailStyles = {
+      color: theColorPurple
+    };
     let social = Object.keys(data).reduce((acc, current) => {
       if (['twitter', 'linkedin', 'github', 'facebook'].indexOf(current) > -1 && this.props.data[current].length > 0) {
         acc[current] = data[current];
@@ -10,13 +18,13 @@ export default class SignatureOutput extends React.Component {
     }, {})
 
     return (
-      <div className="signature-output">
+      <div>
         <h1>Result</h1>
         <p>
-          <span>{name || 'name'}</span> |&nbsp;
+          <span style={nameStyles}>{name || 'name'}</span> |&nbsp;
           <span>{title || 'title'}</span> <br/>
           <span>Heroku | Salesforce | 650 7th Street | San Francisco | CA 94103</span> <br/>
-          <span><a href={"mailto:" + email}>{email || 'email'}</a></span> |&nbsp;
+          <span style={emailStyles}><a href={"mailto:" + email}>{email || 'email'}</a></span> |&nbsp;
           {Object.keys(social).map((site, index) => {
             return <span key={site + index}>{social[site]}&nbsp;</span>;
           })}
